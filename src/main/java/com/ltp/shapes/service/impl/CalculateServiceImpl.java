089e9c4d3e05a05299376256ed4178d9a08e98ca
ALazyGuy <razorprogrammer@gmail.com>
2021-04-09 10:31:57 +0300

    feat: Added util usage

--- src/main/java/com/ltp/shapes/service/impl/CalculateServiceImpl.java
+++ src/main/java/com/ltp/shapes/service/impl/CalculateServiceImpl.java
@@ -1,9 +1,9 @@
 package com.ltp.shapes.service.impl;
 
 import com.ltp.shapes.exception.ShapeException;
-import com.ltp.shapes.model.entity.Point;
 import com.ltp.shapes.model.entity.Triangle;
 import com.ltp.shapes.service.CalculateService;
+import com.ltp.shapes.util.Util;
 import com.ltp.shapes.validator.ShapeValidator;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -26,9 +26,9 @@ public class CalculateServiceImpl implements CalculateService {
         if(!ShapeValidator.validateTriangle(triangle)){
             throw new ShapeException("Invalid input triangle");
         }
-        double a = calculateDistance(triangle.getP1(), triangle.getP2());
-        double b = calculateDistance(triangle.getP2(), triangle.getP3());
-        double c = calculateDistance(triangle.getP3(), triangle.getP1());
+        double a = Util.calculateDistance(triangle.getP1(), triangle.getP2());
+        double b = Util.calculateDistance(triangle.getP2(), triangle.getP3());
+        double c = Util.calculateDistance(triangle.getP3(), triangle.getP1());
         double p = (a + b + c) / 2;
         double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return result;
@@ -39,16 +39,11 @@ public class CalculateServiceImpl implements CalculateService {
         if(!ShapeValidator.validateTriangle(triangle)){
             throw new ShapeException("Invalid input triangle");
         }
-        double result = calculateDistance(triangle.getP1(), triangle.getP2());
-        result += calculateDistance(triangle.getP2(), triangle.getP3());
-        result += calculateDistance(triangle.getP3(), triangle.getP1());
+        double result = Util.calculateDistance(triangle.getP1(), triangle.getP2());
+        result += Util.calculateDistance(triangle.getP2(), triangle.getP3());
+        result += Util.calculateDistance(triangle.getP3(), triangle.getP1());
         return result;
     }
 
-    private double calculateDistance(Point p1,Point p2){
-        double dx = Math.abs(p1.getX() - p2.getX());
-        double dy = Math.abs(p1.getY() - p2.getY());
-        double distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-        return distance;
-    }
+
 }
