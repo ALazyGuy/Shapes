e68c65850d056100e069fe9257e1f4fc73803847
ALazyGuy <razorprogrammer@gmail.com>
2021-04-09 10:20:58 +0300

    fix: Fixed validation bug

--- src/main/java/com/ltp/shapes/validator/ShapeValidator.java
+++ src/main/java/com/ltp/shapes/validator/ShapeValidator.java
@@ -9,9 +9,11 @@ public class ShapeValidator {
 
     public static boolean validateTriangle(Triangle triangle){
         boolean notNull = triangle != null && triangle.getP1() != null && triangle.getP2() != null && triangle.getP3() != null;
-        boolean isNotValid = triangle.getP1().getX() == triangle.getP2().getX() && triangle.getP1().getX() == triangle.getP3().getX();
-        isNotValid = isNotValid && triangle.getP1().getY() == triangle.getP2().getY() && triangle.getP1().getY() == triangle.getP3().getY();
-        return notNull && !isNotValid
+        if(notNull){
+            notNull = notNull && !(triangle.getP1().getX() == triangle.getP2().getX() && triangle.getP1().getX() == triangle.getP3().getX());
+            notNull = notNull && !(triangle.getP1().getY() == triangle.getP2().getY() && triangle.getP1().getY() == triangle.getP3().getY());
+        }
+        return notNull;
     }
 
 }
