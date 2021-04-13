6dbb795ee097b2efe1cde338063f3c3bb133269c
ALazyGuy <razorprogrammer@gmail.com>
2021-04-13 10:52:30 +0300

    feat: Added factory method for ShapeFactory

---
+++ src/main/java/com/ltp/shapes/factory/ShapeFactory.java
@@ -0,0 +1,36 @@
+package com.ltp.shapes.factory;
+
+import com.ltp.shapes.exception.ShapeException;
+import com.ltp.shapes.model.entity.Point;
+import com.ltp.shapes.model.entity.Triangle;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ShapeFactory {
+
+    private static ShapeFactory instance;
+    private static int idCounter = 0;
+
+    public static ShapeFactory getInstance(){
+        if(instance == null){
+            instance = new ShapeFactory();
+        }
+
+        return instance;
+    }
+
+    public Triangle createShape(String name, int ...args) throws ShapeException {
+        if(name.equals("triangle")){
+            return createTriangle(args[0], args[1], args[2], args[3], args[4], args[5]);
+        }
+        throw new ShapeException("Unknown shape type");
+    }
+
+    private Triangle createTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
+        Point p1 = new Point(x1, y1), p2 = new Point(x2, y2), p3 = new Point(x3, y3);
+        Triangle result = new Triangle(p1, p2, p3, idCounter++);
+        return result;
+    }
+
+}
