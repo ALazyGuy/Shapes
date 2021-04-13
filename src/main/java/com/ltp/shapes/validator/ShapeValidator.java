e9a685b9a2c1ba6a44941f5cc3041563d972eaf3
ALazyGuy <razorprogrammer@gmail.com>
2021-04-13 10:13:54 +0300

    feat: Added line validation

--- src/main/java/com/ltp/shapes/validator/ShapeValidator.java
+++ src/main/java/com/ltp/shapes/validator/ShapeValidator.java
@@ -4,8 +4,12 @@ import com.ltp.shapes.model.entity.Triangle;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.regex.Pattern;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ShapeValidator {
+
+    private static final String VALID_EXP = "((\\d+\\s+\\d)(,|$)){3}";
 
     public static boolean validateTriangle(Triangle triangle){
         boolean notNull = triangle != null && triangle.getP1() != null && triangle.getP2() != null && triangle.getP3() != null;
@@ -14,6 +18,11 @@ public class ShapeValidator {
             notNull = notNull && !(triangle.getP1().getY() == triangle.getP2().getY() && triangle.getP1().getY() == triangle.getP3().getY());
         }
         return notNull;
+    }
+
+    public static boolean validateLine(String line){
+        boolean isValid = Pattern.matches(VALID_EXP, line);
+        return isValid;
     }
 
 }
