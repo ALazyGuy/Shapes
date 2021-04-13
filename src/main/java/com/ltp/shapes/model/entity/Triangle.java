6d3cd2cb9be4b3bb40279bfeda0de6caf4cfc735
ALazyGuy <razorprogrammer@gmail.com>
2021-04-13 09:58:15 +0300

    feat: Added id field for Triangle

--- src/main/java/com/ltp/shapes/model/entity/Triangle.java
+++ src/main/java/com/ltp/shapes/model/entity/Triangle.java
@@ -8,11 +8,11 @@ import lombok.Data;
 public class Triangle {
 
     private Point p1, p2, p3;
-    private final String NAME = "Triangle";
+    private int id;
 
     @Override
     public String toString(){
-        return String.format("%s -> { %s, %s, %s }", NAME, p1.toString(), p2.toString(), p3.toString());
+        return String.format("%d -> { %s, %s, %s }", id, p1.toString(), p2.toString(), p3.toString());
     }
 
     @Override
@@ -20,12 +20,12 @@ public class Triangle {
         if(o == this) return true;
         if(o == null || o.getClass() != getClass()) return false;
         Triangle shape = (Triangle)o;
-        return p1.equals(shape.p1) && p2.equals(shape.p2) && p3.equals(shape.p3);
+        return p1.equals(shape.p1) && p2.equals(shape.p2) && p3.equals(shape.p3) && shape.getId() == id;
     }
 
     @Override
     public int hashCode(){
-        int result = 19 * p1.hashCode() + 13 * p2.hashCode() + 31 * p3.hashCode();
+        int result = 19 * p1.hashCode() + 13 * p2.hashCode() + 31 * p3.hashCode() - id;
         return result;
     }
 
